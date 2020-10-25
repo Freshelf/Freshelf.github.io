f2c595dd7e4e6dc374ec372eecd35b5a37283abf
Phili_OST <postr032@uottawa.ca>
2020-10-24 22:24:18 -0400

    navbar now appears and disappears with scrolling

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+window.onscroll = function() { scrolling() };
+var navbar = document.getElementById("navbar");
 
+function scrolling() {
+    if (document.body.scrollTop > window.innerHeight - window.innerWidth*0.055 || document.documentElement.scrollTop > window.innerHeight - window.innerWidth*0.055) {
+        navbar.style.top = "0";
+    } else {
+        navbar.style.top = "-5.5vw";
+    }
+}
