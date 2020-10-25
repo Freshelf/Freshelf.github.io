136ca5a044628e400f82f21e8cec260fccfff393
Phili_OST <postr032@uottawa.ca>
2020-10-25 17:14:38 -0400

    Made navbar slightly smaller

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ window.onscroll = function() { scrolling() };
 var navbar = document.getElementById("navbar");
 
 function scrolling() {
-    if (document.body.scrollTop > window.innerHeight - window.innerWidth*0.055 || document.documentElement.scrollTop > window.innerHeight - window.innerWidth*0.055) {
+    if (document.body.scrollTop > window.innerHeight - window.innerWidth*0.04 || document.documentElement.scrollTop > window.innerHeight - window.innerWidth*0.04) {
         navbar.style.top = "0";
     } else {
         navbar.style.top = "-5.5vw";
