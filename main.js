605eb02141a744431c524ab65832794bf0456d82
Phili_OST <postr032@uottawa.ca>
2020-10-25 17:38:03 -0400

    Changed navbar animation and fixed minor visual issue

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var navbar = document.getElementById("navbar");
 function scrolling() {
     if (document.body.scrollTop > window.innerHeight - window.innerWidth*0.04 || document.documentElement.scrollTop > window.innerHeight - window.innerWidth*0.04) {
         navbar.style.top = "0";
+        navbar.style.opacity = "1";
     } else {
-        navbar.style.top = "-5.5vw";
+        navbar.style.top = "-2vw";
+        navbar.style.opacity = "0";
     }
 }
